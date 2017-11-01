b055172d22d639ffdaaab495f628392230b5cbd9
SRINUSUNKARA <srenu.satyavani@gmail.com>
2017-11-01 10:36:56 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld
 {
    public static void main(String args[])
    {
-      System.out.println("Hello Guys good day and welcome to DevOps session");
+      System.out.println("Hello Guys good day and welcome to DevOps session with poll scm concept");
    }
 }
